7dfe056d3066ae2a3860dd9fc2555e4c836b78e0
Tomasz Grzesiak <grzesiaktomasz@gmail.com>
2025-01-20 15:11:44 +0400

    feat: form error handling

--- UNO/src/client/src/views/GameSetup.vue
+++ UNO/src/client/src/views/GameSetup.vue
@@ -1,29 +1,75 @@
 <script setup lang="ts">
+import { startGameFormSchema } from '@/schemas/startGameFormSchema'
 import { useGameStore } from '@/stores/gameStore'
-import { computed, reactive } from 'vue'
+import { computed, reactive, ref } from 'vue'
+import { z } from 'zod'
+
+type FormData = z.infer<typeof startGameFormSchema>
+type FormErrors = Partial<Record<keyof FormData, string>>
 
 const gameStore = useGameStore()
 
-const form = reactive({
+const form = reactive<{
+  userName: string
+  targetScore: number
+  cardsPerPlayer: number
+  bots: { name: string }[]
+}>({
   userName: '',
   targetScore: 500,
   cardsPerPlayer: 7,
+  bots: [],
 })
 
+const errors = ref<FormErrors>({})
+
 const handleSubmit = async () => {
-  const props = {
-    players: ['a', 'b'],
+  const formData = {
+    userName: form.userName,
     targetScore: form.targetScore,
     cardsPerPlayer: form.cardsPerPlayer,
+    bots: form.bots,
   }
 
   try {
+    startGameFormSchema.parse(formData)
+
+    errors.value = {}
+
+    const players = [formData.userName, ...formData.bots.map((bot) => bot.name)].filter(
+      (name) => name !== '',
+    )
+
+    const props = {
+      players,
+      targetScore: formData.targetScore,
+      cardsPerPlayer: formData.cardsPerPlayer,
+    }
+
     gameStore.initializeGame(props)
   } catch (error) {
-    console.error('Error fetching job', error)
+    if (error instanceof z.ZodError) {
+      errors.value = error.errors.reduce<Record<string, string>>((acc, curr) => {
+        const fieldPath = curr.path.join('.')
+        acc[fieldPath] = curr.message
+        return acc
+      }, {})
+    }
   }
 }
+
+const addBot = () => {
+  if (form.bots.length < 3) {
+    form.bots.push({ name: '' })
+  }
+}
+
+const removeBot = (index: number) => {
+  form.bots.splice(index, 1)
+}
+
 const playerCount = computed(() => gameStore.playerCount)
+console.log(computed(() => gameStore.playerCount))
 </script>
 
 <template>
@@ -44,6 +90,7 @@ const playerCount = computed(() => gameStore.playerCount)
           placeholder="eg. bestplayer123"
           required
         />
+        <p v-if="errors.userName" class="text-red-500">{{ errors.userName }}</p>
       </div>
 
       <div class="mb-4">
@@ -55,6 +102,7 @@ const playerCount = computed(() => gameStore.playerCount)
           name="targetScore"
           class="border rounded w-full py-2 px-3 mb-2"
         />
+        <p v-if="errors.targetScore" class="text-red-500">{{ errors.targetScore }}</p>
       </div>
 
       <div class="mb-4">
@@ -68,17 +116,57 @@ const playerCount = computed(() => gameStore.playerCount)
           name="cardsPerPlayer"
           class="border rounded w-full py-2 px-3 mb-2"
         />
+        <p v-if="errors.cardsPerPlayer" class="text-red-500">
+          {{ errors.cardsPerPlayer }}
+        </p>
+      </div>
+
+      <div class="mb-4">
+        <label class="block text-gray-700 font-bold mb-2">Bots</label>
+        <div v-for="(bot, index) in form.bots" :key="index" class="mb-2">
+          <div class="flex items-center mb-2">
+            <input
+              type="text"
+              v-model="bot.name"
+              class="border rounded w-full py-2 px-3 mr-2"
+              :placeholder="'Bot ' + (index + 1) + ' name'"
+              required
+            />
+
+            <button
+              type="button"
+              class="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline"
+              @click="removeBot(index)"
+            >
+              Delete
+            </button>
+          </div>
+          <p v-if="(errors as any)[`bots.${index}.name`]" class="text-red-500">
+            {{ (errors as any)[`bots.${index}.name`] }}
+          </p>
+        </div>
+        <p v-if="errors.bots" class="text-red-500">{{ errors.bots }}</p>
+
+        <button
+          type="button"
+          class="text-primary-700 border-primary-500 border font-bold py-2 px-4 rounded-full mt-2 focus:outline-none focus:shadow-outline disabled:cursor-not-allowed"
+          @click="addBot"
+          :disabled="form.bots.length >= 3"
+        >
+          Add Bot
+        </button>
       </div>
 
       <div>
         <button
-          class="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline"
+          class="bg-primary-500 hover:bg-primary-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline"
           type="submit"
         >
           Start Game
         </button>
       </div>
     </form>
+
     <div v-if="playerCount > 0">
       <h2 class="text-2xl text-center font-semibold mb-6">Players</h2>
       <p>{{ playerCount }}</p>
