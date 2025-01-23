da833d2f267b14f3075f6ab2bc9d0ac9d2699250
Tomasz Grzesiak <grzesiaktomasz@gmail.com>
2025-01-23 14:59:51 +0400

    feat: game flow improvements

--- UNO/src/client/src/components/ScoreBoard.vue
+++ UNO/src/client/src/components/ScoreBoard.vue
@@ -13,18 +13,21 @@ const scores = computed(() =>
 </script>
 
 <template>
-  <table class="w-96 table-auto border-collapse border border-border text-center">
-    <thead>
-      <tr>
-        <th class="border border-borderHover px-4 py-2 font-bold">Player</th>
-        <th class="border border-borderHover px-4 py-2 font-bold">Score</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="player in scores" :key="player.name">
-        <td class="border border-borderHover px-4 py-2">{{ player.name }}</td>
-        <td class="border border-borderHover px-4 py-2">{{ player.score }}</td>
-      </tr>
-    </tbody>
-  </table>
+  <div>
+    <p class="text-left">Scoreboard</p>
+    <table class="w-96 table-auto border-collapse border border-border text-center">
+      <thead>
+        <tr>
+          <th class="border border-borderHover px-4 py-2 font-bold">Player</th>
+          <th class="border border-borderHover px-4 py-2 font-bold">Score</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="player in scores" :key="player.name">
+          <td class="border border-borderHover px-4 py-2">{{ player.name }}</td>
+          <td class="border border-borderHover px-4 py-2">{{ player.score }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
