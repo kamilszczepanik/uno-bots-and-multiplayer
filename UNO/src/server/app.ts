f0570938390e1396cf5d4d4785d93ce86a553544
Kamil Szczepanik <kszczepanikworkspace@gmail.com>
2025-03-07 12:58:36 +0400

    feat: add and setup nodemon

---
+++ UNO/src/server/app.ts
@@ -0,0 +1,22 @@
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import authRoutes from './routes/authRoutes'
+
+dotenv.config()
+
+const app: Application = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/auth', authRoutes)
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('UNO Multiplayer Server is running!')
+})
+
+const PORT: number = 8000
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`)
+})
