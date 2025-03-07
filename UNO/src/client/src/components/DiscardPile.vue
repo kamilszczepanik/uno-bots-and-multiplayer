e45d78ec45f4990a8e76267adece2bb969dbab9f
Kamil Szczepanik <kszczepanikworkspace@gmail.com>
2025-03-07 12:58:36 +0400

    feat: bots can change color using wild card, info about that color and bots can draw and play the draw card

--- UNO/src/client/src/components/DiscardPile.vue
+++ UNO/src/client/src/components/DiscardPile.vue
@@ -5,10 +5,20 @@ import { useGameStore } from '@/stores/gameStore'
 
 const gameStore = useGameStore()
 const topCard = computed(() => gameStore.currentHand?.discardPile().top())
+const newColor = computed(() => gameStore.currentHand?.newColor)
 </script>
 
 <template>
   <div>
     <GameCard :card="topCard" />
+    <p
+      :class="
+        newColor && topCard && (topCard.type === 'WILD' || topCard.type === 'WILD DRAW')
+          ? 'visible'
+          : 'invisible'
+      "
+    >
+      New color: <span class="font-bold">{{ newColor || ' ' }}</span>
+    </p>
   </div>
 </template>
--- UNO/src/services/BotService.ts
+++ UNO/src/services/BotService.ts
@@ -1,22 +1,48 @@
+import { showMessage } from "../client/src/utils/helpers";
+import { colors } from "../model/deck";
 import { Hand } from "../model/hand";
 import { Game } from "../model/uno";
 
 const BotService = {
-  takeTurn(currentHand: Hand | undefined, botIndex: number) {
-    console.log(botIndex);
-    const botHand = currentHand?.playerHand(botIndex);
+  async takeTurn(currentHand: Hand | undefined, botIndex: number) {
+    if (!currentHand) return;
 
-    if (!currentHand || !botHand) return;
+    const botHand = currentHand.playerHand(botIndex);
+    if (!botHand) return;
 
-    const playableIndex = botHand.findIndex((_, index) =>
+    let playableIndex = botHand.findIndex((_, index) =>
       currentHand.canPlay(index)
     );
 
-    if (playableIndex !== -1) {
-      currentHand.play(playableIndex);
-    } else {
+    if (playableIndex === -1) {
       currentHand.draw();
+
+      const newCardIndex = botHand.length - 1;
+      if (currentHand.canPlay(newCardIndex)) {
+        await this.wait(2000);
+        playableIndex = newCardIndex;
+      } else {
+        return;
+      }
     }
+
+    const cardToPlay = botHand[playableIndex];
+
+    if (cardToPlay.type === "WILD" || cardToPlay.type === "WILD DRAW") {
+      const randomColor = this.getRandomColor();
+      currentHand.play(playableIndex, randomColor);
+      showMessage(`The bot played a wild card and chose ${randomColor}`);
+    } else {
+      currentHand.play(playableIndex);
+    }
+  },
+
+  getRandomColor() {
+    return colors[Math.floor(Math.random() * colors.length)];
+  },
+
+  async wait(ms: number): Promise<void> {
+    return new Promise((resolve) => setTimeout(resolve, ms));
   },
 };
 
