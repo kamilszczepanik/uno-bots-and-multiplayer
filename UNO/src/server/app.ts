4447c2fe09facfb8b5010c2393ff5d8b9bc00b88
Kamil Szczepanik <kszczepanikworkspace@gmail.com>
2025-03-07 12:58:36 +0400

    feat: sqlite db setup, dev script, server running

--- UNO/src/server/app.ts
+++ UNO/src/server/app.ts
@@ -1,2 +1,18 @@
-const express = require("express");
-const app = express();
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+import { PrismaClient } from "@prisma/client";
+
+const app: Application = express();
+const prisma = new PrismaClient();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("UNO Multiplayer Server is running!");
+});
+
+const PORT: number = 8000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
