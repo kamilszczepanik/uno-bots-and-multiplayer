44d69eee549b6eb3af75ba72d8708facceb6f890
Kamil Szczepanik <kszczepanikworkspace@gmail.com>
2025-03-07 12:58:35 +0400

    feat: player info

--- UNO/src/client/src/components/OpponentHand.vue
+++ UNO/src/client/src/components/OpponentHand.vue
@@ -1,18 +1,23 @@
 <script setup lang="ts">
 import { computed, defineProps } from 'vue'
 import GameCard from './GameCard.vue'
-import type { Card } from '../../../model/deck'
+import { useGameStore } from '@/stores/gameStore'
+import PlayerInfo from './PlayerInfo.vue'
 
-const { opponentHand, placement } = defineProps({
-  opponentHand: {
-    type: Array as () => Card[],
-    required: false,
+const { opponentIndex, placement } = defineProps({
+  opponentIndex: {
+    type: Number,
+    required: true,
   },
   placement: {
     type: String as () => 'top' | 'left' | 'right',
     required: true,
   },
 })
+
+const gameStore = useGameStore()
+
+const opponentHand = computed(() => gameStore.currentHand?.playerHand(opponentIndex) ?? [])
 
 const rotation = computed(() => {
   switch (placement) {
@@ -27,13 +32,13 @@ const rotation = computed(() => {
   }
 })
 const spacing = computed(() => {
-  if (!opponentHand) {
+  if (!opponentHand.value) {
     return 0
   }
 
   const defaultSpacing = 200
   const tightSpacing = 100
-  const cardCount = opponentHand.length
+  const cardCount = opponentHand.value.length
   const maxSpacing = placement === 'top' ? defaultSpacing : tightSpacing
 
   return Math.min(100, maxSpacing / cardCount)
@@ -41,16 +46,28 @@ const spacing = computed(() => {
 </script>
 
 <template>
-  <div :class="`relative flex ${rotation} ${placement === 'top' ? 'h-32' : 'w-56'} px-6`">
+  <div
+    v-if="opponentHand.length > 0"
+    :class="`flex ${placement !== 'top' ? 'flex-col' : 'flex-row'} items-center`"
+  >
+    <PlayerInfo :playerIndex="opponentIndex" />
+
     <div
-      v-for="(card, index) in opponentHand"
-      :key="index"
-      :class="`absolute`"
+      :class="`relative flex ${rotation} ${placement === 'top' ? 'h-32' : 'w-56'} px-6`"
       :style="{
-        transform: `translate(${index * spacing}px, ${index * 0}px)`,
+        height: placement !== 'top' ? `${Math.max(200, opponentHand.length * 20)}px` : '',
       }"
     >
-      <GameCard :showBack="true" />
+      <div
+        v-for="(card, index) in opponentHand"
+        :key="index"
+        :class="`absolute`"
+        :style="{
+          transform: `translateX(${index * spacing}px)`,
+        }"
+      >
+        <GameCard :showBack="true" />
+      </div>
     </div>
   </div>
 </template>
---
+++ UNO/src/client/src/components/PlayerInfo.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { useGameStore } from '@/stores/gameStore'
+import { computed } from 'vue'
+
+const { playerIndex } = defineProps({
+  playerIndex: {
+    type: Number,
+    required: true,
+  },
+})
+
+const gameStore = useGameStore()
+const score = computed(() => gameStore.gameInstance?.score(playerIndex))
+const name = computed(() => gameStore.gameInstance?.player(playerIndex))
+</script>
+
+<template>
+  <div class="m-4 flex flex-col">
+    <h3 class="text-lg font-bold">{{ name }}</h3>
+    <p class="text-sm text-gray-600">Score: {{ score }}</p>
+  </div>
+</template>
