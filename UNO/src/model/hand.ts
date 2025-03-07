8a7ec1e82b85b9aeb78da0aef26473e4592fb8a0
Kamil Szczepanik <kszczepanikworkspace@gmail.com>
2025-03-07 12:58:34 +0400

    feat: implementation of players hands

--- UNO/src/model/hand.ts
+++ UNO/src/model/hand.ts
@@ -15,16 +15,46 @@ export class Hand {
   private _ended: boolean;
   private _dealer: number;
   private _players: string[];
+  private _deck: deck.Deck;
+  private _playerHands: deck.Card[][];
 
   constructor({
     players = ["A", "B", "C", "D"],
     dealer = 0,
-    shuffler: standardShuffler,
+    shuffler = standardShuffler,
+    cardsPerPlayer = 7,
   }: Props) {
+    if (players.length < 2 || players.length > 10) {
+      throw new Error(
+        "A game requires at least 2 players and allows at most 10 players."
+      );
+    }
+
     this._players = players;
     this._score = 0;
     this._ended = false;
     this._dealer = dealer;
+
+    this._deck = deck.createInitialDeck();
+    this._deck.shuffle(shuffler);
+
+    this._playerHands = players.map(() => []);
+    for (let i = 0; i < cardsPerPlayer; i++) {
+      for (let p = 0; p < players.length; p++) {
+        const card = this._deck.deal();
+        if (card) {
+          this._playerHands[p].push(card);
+        }
+      }
+    }
+  }
+
+  get dealer() {
+    return this._dealer;
+  }
+
+  get playerCount() {
+    return this._players.length;
   }
 
   draw() {}
@@ -32,6 +62,19 @@ export class Hand {
   play(cardNumber: number) {
     console.log("Playing card", cardNumber); // todo: implement this
     this._ended = true;
+  }
+
+  player(playerNumber: number) {
+    if (playerNumber < 0 || playerNumber > this._players.length - 1)
+      throw new Error("Requested player is out of bounds.");
+    return this._players[playerNumber];
+  }
+
+  playerHand(playerNumber: number): deck.Card[] {
+    if (playerNumber < 0 || playerNumber >= this._players.length) {
+      throw new Error("Requested player is out of bounds.");
+    }
+    return this._playerHands[playerNumber];
   }
 
   hasEnded() {
@@ -45,16 +88,12 @@ export class Hand {
   score() {
     return this._score;
   }
-
-  get dealer() {
-    return this._dealer;
-  }
 }
 
 export function createHand(props: Partial<Props>): Hand {
   const defaultProps: Props = {
     players: ["A", "B"],
-    dealer: 500,
+    dealer: 0,
     shuffler: standardShuffler,
     cardsPerPlayer: 7,
   };
