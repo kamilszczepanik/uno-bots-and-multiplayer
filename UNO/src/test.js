48f3db86652ae15345ab82bb499540ca12d14a1d
Kamil Szczepanik <kszczepanikworkspace@gmail.com>
2025-03-07 12:58:34 +0400

    setup: added test from the teacher to the main folder of assignment. Test are runnig.

--- UNO/src/test.js
+++
@@ -1,3 +0,0 @@
-var zmienna1 = "Test string 1"; //Dodanie :string sprawia, ze zaczynamy uzywac typescript, w przeciwiestwie do podstawowego javascript
-console.log("Test + ".concat(zmienna1));
-console.log("Test + ", zmienna1);
--- UNO/src/test.ts
+++
@@ -1,4 +0,0 @@
-const zmienna1: string = "Test string 1"; //Dodanie :string sprawia, ze zaczynamy uzywac typescript, w przeciwiestwie do podstawowego javascript
-
-console.log(`Test + ${zmienna1}`);
-console.log("Test + ", zmienna1);
