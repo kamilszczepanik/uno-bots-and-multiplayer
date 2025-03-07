99809e7ea907cb411b099924c8e3c0b07deb9fc3
Kamil Szczepanik <kszczepanikworkspace@gmail.com>
2025-03-07 12:58:35 +0400

    feat: card mapper and game card is working

--- UNO/src/client/src/components/GameCard.vue
+++ UNO/src/client/src/components/GameCard.vue
@@ -1,19 +1,27 @@
 <script setup lang="ts">
-import { defineProps } from 'vue'
+import { computed, defineProps } from 'vue'
 import type { Card } from '../../../model/deck'
+import { getCardImagePath } from '../../../utils/cardMapper'
 
-defineProps({
+const { card } = defineProps({
   card: {
     type: Object as () => Card,
   },
 })
+
+const cardImageSrc = computed(() =>
+  card
+    ? new URL(getCardImagePath(card), import.meta.url).href
+    : new URL('../assets/cards/Unknown.png', import.meta.url).href,
+)
 </script>
 
 <template>
-  <div :class="`p-6 rounded-lg shadow-md`">
-    <div v-if="!card">Card is loading</div>
-    <div v-else>
-      <h2 class="text-2xl font-bold mb-4">{{ card?.number }}</h2>
-    </div>
+  <div :class="`w-32 h-48 rounded-lg shadow-md`">
+    <img
+      :src="cardImageSrc"
+      :alt="`Card ${card?.type ?? 'Unknown'}`"
+      class="w-full h-full object-cover rounded-lg"
+    />
   </div>
 </template>
